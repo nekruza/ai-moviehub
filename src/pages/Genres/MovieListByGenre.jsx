f0d7d763c5cec15f0947b3a4ae8abc5808a60a16
Nekruz Avgani <nekruzavgani@Nekruzs-MacBook-Air.local>
2022-08-13 21:36:30 +0100

    added link to each genre

--- src/pages/Genres/MovieListByGenre.jsx
+++ src/pages/Genres/MovieListByGenre.jsx
@@ -39,32 +39,32 @@ const MovieListByGenre = () => {
                 .filter((item, key, array) => filterList(item, array))
                 .map((item) => (
                     <Grid key={item.id} item xs={6} sm={4} md={3} lg={2} p={1} >
-                        <Card sx={{
-                            height: 300,
-                            // minWidth: 200,
-                            display: 'flex',
-                            justifyContent: 'end',
-                            flexDirection: 'column',
-                            backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`,
-                            backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
-                        }}>
-                            <CardContent style={{
-                                background: 'linear-gradient(to top, rgba(0,0,0,0.7) 50%, ' +
-                                    'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-                                color: 'white',
-                                padding: 15
+                        <Link to={`/movie/${item.id}`} style={{ textDecoration: 'none' }}>
+                            <Card sx={{
+                                height: 300,
+                                // minWidth: 200,
+                                display: 'flex',
+                                justifyContent: 'end',
+                                flexDirection: 'column',
+                                backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`,
+                                backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
                             }}>
-                                <Typography gutterBottom variant="h7" component="div" style={{ fontWeight: 800 }}>
-                                    {item.original_title}
-                                </Typography>
-                                <Typography variant="body2"  >
-                                    <strong> IMDB: {item.vote_average}</strong>
-                                </Typography>
-                                <Link to={`/movie/${item.id}`} style={{ textDecoration: 'none' }}>
+                                <CardContent style={{
+                                    background: 'linear-gradient(to top, rgba(0,0,0,0.7) 50%, ' +
+                                        'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                                    color: 'white',
+                                    padding: 15
+                                }}>
+                                    <Typography gutterBottom variant="h7" component="div" style={{ fontWeight: 800 }}>
+                                        {item.original_title}
+                                    </Typography>
+                                    <Typography variant="body2"  >
+                                        <strong> IMDB: {item.vote_average}</strong>
+                                    </Typography>
                                     <Button size="small" variant="contained" color="error">More Info</Button>
-                                </Link>
-                            </CardContent>
-                        </Card>
+                                </CardContent>
+                            </Card>
+                        </Link>
                     </Grid>
                 ))}
         </Grid>
