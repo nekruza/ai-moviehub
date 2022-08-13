c4b8cd8e42f031fbcb8370b851ebd9a5bb47cfb4
Nekruz Avgani <nekruzavgani@Nekruzs-MacBook-Air.local>
2022-08-13 21:32:01 +0100

    added icons to categories

---
+++ src/Icons/Icons.js
@@ -0,0 +1,58 @@
+import Action from '@mui/icons-material/Movie';
+import Adventure from '@mui/icons-material/MovieFilter';
+import Animation from '@mui/icons-material/Animation';
+import Comedy from '@mui/icons-material/TheaterComedy';
+import Crime from '@mui/icons-material/Warning';
+import Drama from '@mui/icons-material/VideoCameraFront';
+import Fantasy from '@mui/icons-material/ElectricBolt';
+import Family from '@mui/icons-material/FamilyRestroom';
+import Horror from '@mui/icons-material/Outlet';
+import History from '@mui/icons-material/Fort';
+
+
+
+export default function Icons() {
+    return (
+        [
+            {
+                name: 'Action',
+                icon: Action
+            },
+            {
+                name: 'Adventure',
+                icon: Adventure
+            },
+            {
+                name: 'Animation',
+                icon: Animation
+            },
+            {
+                name: 'Comedy',
+                icon: Comedy
+            },
+            {
+                name: 'Crime',
+                icon: Crime
+            },
+            {
+                name: 'Drama',
+                icon: Drama
+            },
+            {
+                name: 'Fantasy',
+                icon: Fantasy
+            },
+            {
+                name: 'Family',
+                icon: Family
+            },
+            {
+                name: 'Horror',
+                icon: Horror
+            },
+            {
+                name: 'History',
+                icon: History
+            },
+        ])
+}
