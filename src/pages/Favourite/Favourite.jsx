f0f278c1ca55deca44cf119200bf18f185412003
Nekruz Avgani <nekruzavgani@Nekruzs-MacBook-Air.local>
2022-09-15 12:41:38 +0100

    added transition pop up to cards on Favorite and Watchlist components

--- src/pages/Favourite/Favourite.jsx
+++ src/pages/Favourite/Favourite.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { Box, Grid, Paper } from '@mui/material';
+import { Box, Grid, Paper, Zoom } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -36,34 +36,36 @@ const FavouriteList = () => {
                     <>
                         {data.data.results
                             .map((item) => (
-                                <Grid key={item.id} item xs={6} sm={4} md={3} lg={2} p={1} >
-                                    <Link to={`/movie/${item.id}`} style={{ textDecoration: 'none' }}>
-                                        <Card sx={{
-                                            height: 300,
-                                            // minWidth: 200,
-                                            display: 'flex',
-                                            justifyContent: 'end',
-                                            flexDirection: 'column',
-                                            backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`,
-                                            backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
-                                        }}>
-                                            <CardContent style={{
-                                                background: 'linear-gradient(to top, rgba(0,0,0,0.7) 50%, ' +
-                                                    'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-                                                color: 'white',
-                                                padding: 15
+                                <Zoom in={true}>
+                                    <Grid key={item.id} item xs={6} sm={4} md={3} lg={2} p={1} >
+                                        <Link to={`/movie/${item.id}`} style={{ textDecoration: 'none' }}>
+                                            <Card sx={{
+                                                height: 300,
+                                                // minWidth: 200,
+                                                display: 'flex',
+                                                justifyContent: 'end',
+                                                flexDirection: 'column',
+                                                backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`,
+                                                backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
                                             }}>
-                                                <Typography gutterBottom variant="h7" component="div" style={{ fontWeight: 800 }}>
-                                                    {item.original_title}
-                                                </Typography>
-                                                <Typography variant="body2"  >
-                                                    <strong> IMDB: {item.vote_average}</strong>
-                                                </Typography>
-                                                <Button size="small" variant="contained" color="error">More Info</Button>
-                                            </CardContent>
-                                        </Card>
-                                    </Link>
-                                </Grid>
+                                                <CardContent style={{
+                                                    background: 'linear-gradient(to top, rgba(0,0,0,0.7) 50%, ' +
+                                                        'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                                                    color: 'white',
+                                                    padding: 15
+                                                }}>
+                                                    <Typography gutterBottom variant="h7" component="div" style={{ fontWeight: 800 }}>
+                                                        {item.original_title}
+                                                    </Typography>
+                                                    <Typography variant="body2"  >
+                                                        <strong> IMDB: {item.vote_average}</strong>
+                                                    </Typography>
+                                                    <Button size="small" variant="contained" color="error">More Info</Button>
+                                                </CardContent>
+                                            </Card>
+                                        </Link>
+                                    </Grid>
+                                </Zoom>
                             ))}
                     </>
                     :
--- src/pages/Favourite/Watchlist.jsx
+++ src/pages/Favourite/Watchlist.jsx
@@ -9,6 +9,7 @@ import EmptyState from '../../components/EmptyState';
 import MovieData from '../../api/MovieData';
 import { useQuery } from '@tanstack/react-query'
 import LoadingCircle from '../../components/LoadingCircle';
+import Zoom from '@mui/material/Zoom';
 
 
 const WatchList = () => {
@@ -39,34 +40,36 @@ const WatchList = () => {
                     <>
                         {data.data.results
                             .map((item) => (
-                                <Grid key={item.id} item xs={6} sm={4} md={3} lg={2} p={1} >
-                                    <Link to={`/movie/${item.id}`} style={{ textDecoration: 'none' }}>
-                                        <Card sx={{
-                                            height: 300,
-                                            // minWidth: 200,
-                                            display: 'flex',
-                                            justifyContent: 'end',
-                                            flexDirection: 'column',
-                                            backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`,
-                                            backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
-                                        }}>
-                                            <CardContent style={{
-                                                background: 'linear-gradient(to top, rgba(0,0,0,0.7) 50%, ' +
-                                                    'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-                                                color: 'white',
-                                                padding: 15
+                                <Zoom in={true}>
+                                    <Grid key={item.id} item xs={6} sm={4} md={3} lg={2} p={1} >
+                                        <Link to={`/movie/${item.id}`} style={{ textDecoration: 'none' }}>
+                                            <Card sx={{
+                                                height: 300,
+                                                // minWidth: 200,
+                                                display: 'flex',
+                                                justifyContent: 'end',
+                                                flexDirection: 'column',
+                                                backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`,
+                                                backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
                                             }}>
-                                                <Typography gutterBottom variant="h7" component="div" style={{ fontWeight: 800 }}>
-                                                    {item.original_title}
-                                                </Typography>
-                                                <Typography variant="body2"  >
-                                                    <strong> IMDB: {item.vote_average}</strong>
-                                                </Typography>
-                                                <Button size="small" variant="contained" color="error">More Info</Button>
-                                            </CardContent>
-                                        </Card>
-                                    </Link>
-                                </Grid>
+                                                <CardContent style={{
+                                                    background: 'linear-gradient(to top, rgba(0,0,0,0.7) 50%, ' +
+                                                        'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                                                    color: 'white',
+                                                    padding: 15
+                                                }}>
+                                                    <Typography gutterBottom variant="h7" component="div" style={{ fontWeight: 800 }}>
+                                                        {item.original_title}
+                                                    </Typography>
+                                                    <Typography variant="body2"  >
+                                                        <strong> IMDB: {item.vote_average}</strong>
+                                                    </Typography>
+                                                    <Button size="small" variant="contained" color="error">More Info</Button>
+                                                </CardContent>
+                                            </Card>
+                                        </Link>
+                                    </Grid>
+                                </Zoom>
                             ))}
                     </>
                     :
